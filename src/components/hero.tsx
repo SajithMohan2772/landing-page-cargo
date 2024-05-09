01666fba46f85eb66125c501115bcec71ee2b587
sajith2772 <sajith2772@gmail.com>
2024-05-09 12:19:06 +0530

    comit2

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -20,7 +20,7 @@ const Hero = () => {
     </div>
     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
       <Image
-            src="/solution.jpg"
+            src="/banner.jpg"
             alt="Description of the image"
             width={720}
             height={600}
